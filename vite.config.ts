31d34933b3d1872e1021979ce62499f97d5505b5
Diego Córdova <60157586+Diego-CB@users.noreply.github.com>
2025-10-02 00:57:09 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://github.com/Diego-CB/lyrics-Serenate/", // <-- IMPORTANT
+  base: "https://diego-cb.github.io/lyrics-Serenate/", // <-- IMPORTANT
 });
